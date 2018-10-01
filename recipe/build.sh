da3af97a98bbb7895a43be41a42a62d450b5398f
v-turin <vladimir.turin@opencascade.com>
2018-10-01 22:39:30 +0300

    eigen

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,16 @@
 mkdir build
-pushd build
+cd build
 
 # Configure step.
-cmake G "$CMAKE_GENERATOR" \
-    -D CMAKE_BUILD_TYPE=Release \
-    -D CMAKE_PREFIX_PATH=${PREFIX} \
-    -D CMAKE_INSTALL_PREFIX=${PREFIX} \
-    -D CMAKE_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu \
-    -D OpenGL_GL_PREFERENCE=GLVND \
+cmake -G "$CMAKE_GENERATOR"                                                \
+    -D CMAKE_BUILD_TYPE=Release                                            \
+    -D CMAKE_PREFIX_PATH=${PREFIX}                                         \
+    -D CMAKE_INSTALL_PREFIX=${PREFIX}                                      \
+    -D CMAKE_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu                        \
+    -D OpenGL_GL_PREFERENCE=GLVND                                          \
     ..
 
 # Build step.
-cmake --build . --config Release
-cmake --build . --target install
-
-popd
+make install
+make basicstuff -j${CPU_COUNT}
+ctest -R basicstuff*
