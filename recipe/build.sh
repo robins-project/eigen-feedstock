3d3c99a40b885b4267409dda0738fca6f30e2647
v-turin <vladimir.turin@opencascade.com>
2018-10-15 16:59:52 +0300

    Use native compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,6 @@ cmake -G "$CMAKE_GENERATOR"                                                \
     -D CMAKE_BUILD_TYPE=Release                                            \
     -D CMAKE_PREFIX_PATH=${PREFIX}                                         \
     -D CMAKE_INSTALL_PREFIX=${PREFIX}                                      \
-    -D CMAKE_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu                        \
     -D OpenGL_GL_PREFERENCE=GLVND                                          \
     ..
 
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -z "${CXX}" ]; then
+    CXX=g++
+fi
+
 ${CXX} -I${PREFIX}/include -o test test.cc
 
 ./test
