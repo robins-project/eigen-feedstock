5996a169f1151cf3f5211f1f89478a6d975ae400
v-turin <vladimir.turin@opencascade.com>
2018-11-08 14:43:35 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,12 @@ mkdir build
 cd build
 
 # Configure step.
-cmake -G "$CMAKE_GENERATOR"                                                \
-    -D CMAKE_BUILD_TYPE=Release                                            \
-    -D CMAKE_PREFIX_PATH=${PREFIX}                                         \
-    -D CMAKE_INSTALL_PREFIX=${PREFIX}                                      \
-    -D OpenGL_GL_PREFERENCE=GLVND                                          \
+cmake -G "$CMAKE_GENERATOR"                         \
+    -D CMAKE_BUILD_TYPE=Release                     \
+    -D CMAKE_PREFIX_PATH=${PREFIX}                  \
+    -D CMAKE_INSTALL_PREFIX=${PREFIX}               \
+    -D CMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu  \
+    -D OpenGL_GL_PREFERENCE=GLVND                   \
     ..
 
 # Build step.
